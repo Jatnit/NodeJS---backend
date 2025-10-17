93ddbae3daf792fff9bb4f6f01d2e840b54ae0d4
Jatnit <152699505+Jatnit@users.noreply.github.com>
2025-10-17 16:50:53 +0700

    up backend 1.1
    export Port

--- src/server.js
+++ src/server.js
@@ -1,15 +1,18 @@
 import express from "express";
 import configViewEngine  from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
+require("dotenv").config();
 
 const app = express();
+const PORT = process.env.PORT || 8080;
+
 //config view engine
 configViewEngine(app)
 //init web routes
 initWebRoutes(app)
 
 
-const PORT = 8080
+
 app.listen(PORT, () =>{
     // Sửa lại thành 'PORT'
     console.log(`Example app listening on port ${PORT}`); 
